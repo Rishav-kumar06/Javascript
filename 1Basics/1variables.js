b88c76b02e9eb1fa9db5b276a80d9c368caadb82
Rishav-kumar06 <rishavkumar06801@gmail.com>
2026-03-12 02:43:59 +0530

    add javascript datatype example

---
+++ 1Basics/1variables.js
@@ -0,0 +1,20 @@
+const accountId = 1254
+let accountEmail = "Rishav@gmail.com"
+var accountPassword = "123456"
+accountCity ="Rajkot"
+let accountState;
+// accountId =22   (not allowed)
+
+accountEmail ="ridhsdjdnb@gmail.com"
+accountPassword ="75421865"
+accountCity ="samastipur"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+
+*/
---
+++ README.md
@@ -0,0 +1,2 @@
+# Starting of javascript
+
