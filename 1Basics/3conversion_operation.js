46bf99410dd6ce457dad7666ef8058b7b1ca73e3
Rishav-kumar06 <rishavkumar06801@gmail.com>
2026-03-14 10:04:09 +0530

    Conversion of Datatypes

---
+++ 1Basics/3conversion_operation.js
@@ -0,0 +1,27 @@
+let score ="33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+
+
+// //"33" =>33
+// // "33aa"=>NaN
+// //true => 1; false=>0
+
+let isLoggedIn=1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=>true; 0=> false
+//""=>false
+//"Rishav"=>true
+
+
+let someNumber =18
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
